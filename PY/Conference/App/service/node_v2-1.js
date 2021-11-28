f4087868eb293f982d93c5a185219f7b0e8cac09
viki-mish <62746128+viki-mish@users.noreply.github.com>
2021-11-28 23:55:09 +0530

    removed "onlyunique" function

---
+++ PY/Conference/App/service/node_v2-1.js
@@ -0,0 +1,45 @@
+const port = 3000
+const express = require('express')
+const { exec } = require('child_process');
+const cors = require('cors')
+var http = require('http');
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/mydb";
+var fs = require('fs');
+const app = express()
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
+var issue2options = {
+    origin: true,
+    methods: ['POST'],
+    credentials: true,
+    maxAge: 3600
+};
+let dbo;
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    console.log("Database created!");
+    dbo = db.db("mydb");
+    var myobj = JSON.parse(fs.readFileSync('./conf_ACM.json', 'utf8'));
+    myobj.map((obj) => {
+        dbo.collection("customers").updateOne(obj,{ $set: obj},{upsert:true});
+    })
+});
+
+
+
+
+
+
+
+
+
+app.listen(port, function(err) {
+    if (err) {
+        console.log(err)
+
+    }
+    console.log('server is running on port: ', port)
+})
